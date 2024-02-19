c503586a35210f5e577db294ab817587cf7da5e8
kenresoft <amadikenneth8@gmail.com>
2024-02-19 22:46:34 +0100

    Bug fixes and performance improvents

--- js/main.js
+++ js/main.js
@@ -47,20 +47,20 @@
     
     
     // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
+    // $(".navbar-nav a").on('click', function (event) {
+    //     if (this.hash !== "") {
+    //         event.preventDefault();
             
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 1500, 'easeInOutExpo');
+    //         $('html, body').animate({
+    //             scrollTop: $(this.hash).offset().top - 45
+    //         }, 1500, 'easeInOutExpo');
             
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
-        }
-    });
+    //         if ($(this).parents('.navbar-nav').length) {
+    //             $('.navbar-nav .active').removeClass('active');
+    //             $(this).closest('a').addClass('active');
+    //         }
+    //     }
+    // });
 
 
     // Smooth scrolling to section
